0fffe9a4111254b2beffca4c33b305e808566940
jnguyen727 <styliss777@gmail.com>
2023-11-20 17:18:16 -0500

    Add game function and score counter and round counter.

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-
-
+let playerScore = 0;
+let computerScore = 0;
+let roundCount = 0;
 
 function getComputerChoice() {
     let number = Math.floor(Math.random() * 300);
 
     if (number > 100 && number < 200) {
-        return console.log("Rock!");
+        return ("Rock!");
     }
 
     if (number > 200 && number < 300) {
-        return console.log("Paper!");
+        return ("Paper!");
     }
 
     if (number > 0 && number < 100) {
-        return console.log("Scissors!");
+        return ("Scissors!");
     }
 }
 
@@ -21,40 +22,63 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
 
+
+
     if (playerSelection === "rock" && computerSelection === "Paper!") {
+        computerScore += 1;
+        roundCount += 1;
         return console.log("You Lose!");
+    
+
     }
 
     else if (playerSelection === "rock" && computerSelection === "Scissors!") {
+        playerScore += 1;
+        roundCount += 1;
         return console.log("You Win!");
     }
 
     else if (playerSelection === "rock" && computerSelection === "Rock!") {
+        roundCount += 1;
         return console.log("Draw!");
     }
 
     else if (playerSelection === "scissors" && computerSelection === "Paper!") {
+        playerScore += 1;
+        roundCount += 1;
         return console.log("You Win!");
     }
 
     else if (playerSelection === "scissors" && computerSelection === "Scissors!") {
+        roundCount += 1;
         return console.log("Draw!");
     }
 
     else if (playerSelection === "scissors" && computerSelection === "Rock!") {
+        roundCount += 1;
+        computerScore += 1;
         return console.log("You Lose!");
     }
 
     else if (playerSelection === "paper" && computerSelection === "Paper!") {
+        roundCount += 1;
         return console.log("Draw!");
     }
 
     else if (playerSelection === "paper" && computerSelection === "Scissors!") {
+        roundCount += 1;
+        computerScore += 1;
     return console.log("You Lose!");
     }
 
     else if (playerSelection === "paper" && computerSelection === "Rock!") {
+        roundCount += 1;
+        playerScore += 1;
         return console.log("You Win!");
+    }
+
+    else {
+        return console.log("fuck");
     }
 
 
@@ -68,5 +92,37 @@ function playRound(playerSelection, computerSelection) {
 }
 
 
-playRound();
+function game() {
 
+
+  let playerSelection = prompt();
+    console.log(playerSelection);
+    let computerSelection = getComputerChoice();
+    console.log(computerSelection);
+    playRound(playerSelection, computerSelection);
+    console.log("Your score is " + playerScore)
+    console.log("Your opponents score is " + computerScore)
+ 
+ 
+if (playerScore > computerScore && roundCount === 5) {
+    console.log("You win the game!");
+}
+
+if (playerScore < computerScore && roundCount === 5) {
+    console.log("You lose the game!");
+}
+
+if (playerScore === computerScore && roundCount === 5) {
+    console.log("The game is a draw!");
+}
+
+
+
+
+}
+
+game();
+game();
+game();
+game();
+game();
